a932c7fc2a0bd1f4f5fedfe773f62db4d711cc1b
Danilo Chang <ray2501@gmail.com>
2020-03-05 20:31:21 +0800

    Update to 11.35.19

--- MonetDB.spec
+++ MonetDB.spec
@@ -1,5 +1,5 @@
 %define name MonetDB
-%define version 11.35.15
+%define version 11.35.19
 
 # groups of related archs
 %define all_x86 i386 i586 i686
--- build.tcl
+++ build.tcl
@@ -1,8 +1,8 @@
 #!/usr/bin/tclsh
 
 set arch "x86_64"
-set base "MonetDB-11.35.15"
-set fileurl "https://www.monetdb.org/downloads/sources/Nov2019-SP2/$base.tar.xz"
+set base "MonetDB-11.35.19"
+set fileurl "https://www.monetdb.org/downloads/sources/Nov2019-SP3/$base.tar.xz"
 
 set var [list wget $fileurl -O $base.tar.xz]
 exec >@stdout 2>@stderr {*}$var
