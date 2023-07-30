e19c2dcd1a77d751cbf6ce2b0fd6a93799b7603a
Danilo Chang <ray2501@gmail.com>
2023-07-30 11:04:23 +0800

    Update to 11.47.5

--- MonetDB.spec
+++ MonetDB.spec
@@ -1,5 +1,5 @@
 %define name MonetDB
-%define version 11.47.3
+%define version 11.47.5
 
 # groups of related archs
 %define all_x86 i386 i586 i686
--- build.tcl
+++ build.tcl
@@ -1,8 +1,8 @@
 #!/usr/bin/tclsh
 
 set arch "x86_64"
-set base "MonetDB-11.47.3"
-set fileurl "https://www.monetdb.org/downloads/sources/Jun2023/$base.tar.xz"
+set base "MonetDB-11.47.5"
+set fileurl "https://www.monetdb.org/downloads/sources/Jun2023-SP1/$base.tar.xz"
 
 set var [list wget $fileurl -O $base.tar.xz]
 exec >@stdout 2>@stderr {*}$var
